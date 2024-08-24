52b05797d71c7ebe160f1f5cf79feff7b232dd36
Ricardo Markiewicz <ricardo.markiewicz@gmail.com>
2024-08-24 15:00:33 +0200

    Add missing file

---
+++ lib/src/credentials.dart
@@ -0,0 +1,8 @@
+// username: String - Username on Twitch
+// password: String - OAuth password on Twitch (http://twitchapps.com/tmi/)
+class Credentials {
+  final String username;
+  final String password;
+
+  Credentials({required this.username, required this.password});
+}
